475515a9efe9ef76e94d1a03b5aefd141c4783d9
winsert <winsert@163.com>
2017-11-08 14:06:09 +0800

    简化应用

--- ann.py
+++ ann.py
@@ -11,10 +11,7 @@ from datetime import datetime
 
 from cxcb import getCB #查询可转债,可交换债是否满足三线的模块
 from cxhp import getHP #高价折扣模块
-from cxwh import getWH #查询外汇模块
-from cxjj import getJJ #查询基金模块
 from cxqs import getQS #强赎模块
-from cxindex import getIndex #指数模块
 
 if __name__ == '__main__':
     
@@ -27,9 +24,8 @@ if __name__ == '__main__':
     endMsg =  "I will come back !"
     newPriceMsg =  u"没有可转债满足 买入条件。"
     HPriceMsg =  u"没有可转债满足 高价折扣法 。"
-    WHmsg = u"外汇一切正常 !"
-    JJmsg = u"基金一切正常 !"
 
+    # 选定微信接收人
     account = itchat.get_friends()
     for user in account:
         if user['NickName'] == 'Andy':
@@ -54,7 +50,7 @@ if __name__ == '__main__':
 
     while 1:
 
-        # 判断晚间休息时间
+        # 运行时间>= 09:25:59 and <= 14:59:59
         if datetime.now() >= start_time and datetime.now() <= pause_time :
 
             # 判断午体时间
@@ -64,7 +60,7 @@ if __name__ == '__main__':
                 print 
                 itchat.send(restStartMsg, toUserName = userName)
 
-                time.sleep(5500)
+                time.sleep(5500) #午休时长5500秒
 
                 print time.asctime(time.localtime(time.time())) #显示查询时间
                 print restEndMsg
@@ -101,40 +97,10 @@ if __name__ == '__main__':
                     #itchat.send(hpMsg, 'filehelper')
                     itchat.send(hpMsg, toUserName = userName)
 
-            # 查询外汇价格是否低于预设值
-            WHlist = getWH()
-            if len(WHlist) == 0: #没有满足条件的外汇
-                print WHmsg
-                print
-            else:
-                for whMsg in WHlist: #有满足条件的外汇
-                    print whMsg
-                    itchat.send(whMsg, toUserName = userName)
-
-            # 查询基金价格是否低于预设值
-            JJlist = getJJ()
-            if len(JJlist) == 0: #没有满足条件的基金
-                print JJmsg
-                print
-            else:
-                for jjMsg in JJlist: #有满足条件的基金
-                    print jjMsg
-                    itchat.send(jjMsg, toUserName = userName)
-
-            time.sleep(300)  # 延时查询的秒数,300即延时5分钟查询一次。
+            time.sleep(180)  # 延时查询的秒数,180即延时3分钟查询一次。
 
         else:
-            # 查询指数收盘的涨跌情况
-            index = {u'上证50':'sh000016', u'沪深300':'sz399300', u'中证500':'sh000905', u'创业板':'sz399006', u'B股':'sh000003', u'国债':'sh000012'} #要查询的指数代码
-    
-            for k in index.keys():
-                value = index.get(k)
-                index_zz = getIndex(value)
-                index_msg = k+' : '+index_zz
-                print index_msg
-                #itchat.send(index_msg, 'filehelper')
-                itchat.send(index_msg, toUserName = userName)
-
+            
             #计算转债的强赎天数：
             qsMsgList = getQS()
             if len(qsMsgList) > 0:
@@ -145,4 +111,5 @@ if __name__ == '__main__':
             print time.asctime(time.localtime(time.time())) #显示查询时间
             itchat.send(endMsg, toUserName='filehelper')
             itchat.send(endMsg, toUserName = userName)
-            time.sleep(64200)  # 延时64800秒(18小时)。
+
+            time.sleep(64200)  # 延时64800秒(18小时)后再继续运行
